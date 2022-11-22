1c19880ebdbc33039b705893e4c72df1090d593f
Kaiya Takahashi <yaikashi@icloud.com>
2022-11-21 16:48:07 -0800

    add task status and task textfield

--- src/components/Timer/Timer.js
+++ src/components/Timer/Timer.js
@@ -1,6 +1,9 @@
-import React from "react"
+import React, { useState } from "react"
 import "/Users/kaiyatakahashi/Desktop/DevTimeManager/client/src/styles/components.css"
 import { useStopwatch } from 'react-timer-hook';
+import { useForm } from 'react-hook-form';
+import TaskState from './TaskState.js';
+import TextField from '@mui/material/TextField'
 
 function Timer() {
     const {
@@ -13,13 +16,29 @@ function Timer() {
         pause,
         reset,
     } = useStopwatch({ autoStart: false });
+    const [isStarted, setIsStarted] = useState(false);
     return (
-        <div>
-            <span>{hours}</span>:<span>{minutes}</span>:<span>{seconds}</span>
-            <p>{isRunning ? 'Running' : 'Not Running'}</p>
-            <button onClick={start}>Start</button>
-            <button onClick={pause}>Pause</button>
-            <button onClick={reset}>Reset</button>
+        <div id="timer-flex">
+            <div>
+                <br></br>
+                <span>Task: </span>
+                <TextField
+                    label="Task"
+                    variant="outlined"
+                    required
+                />
+            </div>
+            <div>
+                <span>{hours}</span>:<span>{minutes}</span>:<span>{seconds}</span>
+                <p>{isRunning ? 'Running' : 'Not Running'}</p>
+                {
+                    isRunning ? <button onClick={pause}>Pause</button> : <button onClick={start}>Start</button>
+                }
+                <button onClick={reset}>Reset</button>
+            </div>
+            <div>
+                <TaskState></TaskState>
+            </div>
         </div>
       );
 }
