282dd12728c2db8eb449e2b71a3c000dfe2ede83
Kaiya Takahashi <yaikashi@icloud.com>
2022-11-28 16:18:37 -0800

    add scope for google calendar

--- src/components/GoogleButton/Login.js
+++ src/components/GoogleButton/Login.js
@@ -1,7 +1,9 @@
 import React, { useEffect } from 'react';
 import { GoogleLogin } from 'react-google-login';
 import { gapi } from 'gapi-script';
+import Axios from 'axios';
 
+Axios.defaults.withCredentials = true
 const clientId = "252537839800-gisn0d87ekkklurcq00ucglil44tmmpm.apps.googleusercontent.com";
 
 var Login = () => {
@@ -14,14 +16,27 @@ var Login = () => {
         };
         gapi.load('client:auth2', start);
     })
+    const onSuccess = (res) => {
+        console.log("Login successfull",res);
+        const code = res;
+        Axios.post('http://localhost:3001/api/create_tokens', {
+            code: code,
+        }).then((response) => {
+            console.log("This is response", response.data)
+        }).catch((err) => { console.log(err.message)})
+    }
     return (
         <GoogleLogin
             clientId={clientId}
-            onSuccess={(res) => {console.log("Log in successfull", res.profileObj)}}
+            onSuccess={onSuccess}
             onFailure={(res) => {console.log("Log in failed", res)}}
             buttonText={"Login"}
             cookiePolicy={"single_host_origin"}
             isSignedIn={true}
+            // Refresh Token
+            responseType='code'
+            accessType='offline'
+            scope='openid email profile https://www.googleapis.com/auth/calendar'
         />
     )
 }
