d0f1fadecb3ee55a523c21776dcd7c9a6297cf58
Kaiya Takahashi <yaikashi@icloud.com>
2022-12-21 16:57:18 -0800

    add toggle function for iconbutton in Weekly table

--- src/components/WeeklyTable/WeeklyTable.js
+++ src/components/WeeklyTable/WeeklyTable.js
@@ -9,6 +9,9 @@ import  Axios  from 'axios';
 import { useEffect, useState } from 'react';
 
 import EditIcon from '@mui/icons-material/Edit';
+import CancelIcon from '@mui/icons-material/Cancel';
+import DeleteIcon from '@mui/icons-material/Delete';
+import SaveAltIcon from '@mui/icons-material/SaveAlt';
 
 Axios.defaults.withCredentials = true;
 
@@ -18,17 +21,29 @@ function preventDefault(event) {
 
 export default function WeeklyTable() {
     const [rows, setRows] = useState([]);
-    const [disable, setDisable] = useState(new Array(rows.length).fill(false));
+    const [disable, setDisable] = useState([{0: null}]);
     useEffect(() => {
         fetchData()
     }, [])
     const fetchData = async () => {
         Axios.get("http://localhost:3001/tasks/get").then((response) => {
             setRows(response["data"]);
-            setDisable(new Array(rows.length).fill(false))
-            console.log(rows)
+            console.log(response)
+            // const arr = new Array(response["data"][response["data"].length - 1]["task_id"]).fill(false);
+            // setDisable(arr)
+            console.log("this is rows: ",rows)
+            console.log("this is dis: ", disable)
         })
     }
+
+    const handleEdit = (event, row) => {
+        console.log("edit1", "disable:", disable, "c:", row);
+        setDisable({
+            ...disable,
+            [row.id]: row.value
+        })
+    }
+    
     return (
         <div id='weekly-table'>
         <faCoffee></faCoffee>
@@ -51,13 +66,35 @@ export default function WeeklyTable() {
                             <TableCell>{row.time}</TableCell>
                             <TableCell>{row.date.substring(5, 10)}</TableCell>
                             <TableCell>{row["is_finished"]}</TableCell>
-                            <EditIcon
-                                onClick={(event) => {
-                                    console.log(row)
-                                }}
-                            >
-
-                            </EditIcon>
+                            <TableCell>
+                                {
+                                    (disable[row.task_id] === false || disable[row.task_id] === undefined) ?
+                                    <EditIcon
+                                    onClick={(event) => {
+                                        handleEdit(event, {
+                                            id:  row.task_id,
+                                            value: true,
+                                        })
+                                    }}
+                                    >
+                                    </EditIcon> : 
+                                    <div className='icon-box'>
+                                        <CancelIcon
+                                        onClick={(event) => {
+                                            handleEdit(event, {
+                                                id:  row.task_id,
+                                                value: false,
+                                            })
+                                        }}
+                                        >
+                                        </CancelIcon>
+                                        <DeleteIcon>
+                                        </DeleteIcon>
+                                        <SaveAltIcon>
+                                        </SaveAltIcon>
+                                    </div>
+                                }
+                            </TableCell>
                         </TableRow>
                     ))}
                 </TableBody>
